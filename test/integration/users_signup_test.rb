5246798c17fa56f632ea7a6fa911a34ca70c92be
Ubuntu <ubuntu@ip-172-31-15-153.ap-south-1.compute.internal>
2020-11-15 16:32:21 +0000

    Add login and authentication

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,9 +12,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: {name: "Nitish", email: "nitish@example.com", password: "password123", password_confirmation: "password123"}
+      post users_path, params: { user: {name: "Nitish", email: "nitish@example.com", password: "password123", password_confirmation: "password123"} }
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
